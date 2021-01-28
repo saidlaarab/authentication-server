b35398082dc4a4f0d0d5f5dbbba025dcec8df195
Said Laa <laarabsaid@gmail.com>
2021-01-28 16:41:30 +0100

    changed MySQL DB configuration data with those related to the embedded JawsMySQL DB

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,9 @@
-CREATE TABLE IF NOT EXISTS `spring`.`user` (
+CREATE TABLE IF NOT EXISTS `vqa34ov9ga2iumfm`.`user` (
     `username` VARCHAR(45) NOT NULL,
     `password` TEXT NOT NULL,
     PRIMARY KEY (`username`));
 
-CREATE TABLE IF NOT EXISTS `spring`.`otp` (
+CREATE TABLE IF NOT EXISTS `vqa34ov9ga2iumfm`.`otp` (
     `username` VARCHAR(45) NOT NULL,
     `code` VARCHAR(45) NULL,
     PRIMARY KEY (`username`));
